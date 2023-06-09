ae068becd2a6c6ddb83a0032592e750e206816e5
SatioGX <78477842+SatioGX@users.noreply.github.com>
2023-06-09 03:10:17 -0700

    Making form apps for Assignment 1

---
+++ Assignment 1/Ch5 Q14/Form1.cs
@@ -0,0 +1,10 @@
+namespace Ch5_Q14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Assignment 1/Ch6 Q5/Form1.cs
@@ -0,0 +1,10 @@
+namespace Ch6_Q5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
